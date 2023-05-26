6f2970c504ba6138acba6502325bcca768f2d0be
Akogwu <apple@Akogwu-MacBook-Pro.local>
2023-05-26 11:33:13 +0100

    steps

---
+++ src/components/extras.ts
@@ -0,0 +1,23 @@
+export type InputArray = {
+  placeholder: string;
+  name: string;
+};
+
+export const inputArray: InputArray[] = [
+  {
+    placeholder: '0000',
+    name: 'input1',
+  },
+  {
+    placeholder: '0000',
+    name: 'input2',
+  },
+  {
+    placeholder: '0000',
+    name: 'input3',
+  },
+  {
+    placeholder: '0000',
+    name: 'input4',
+  },
+];
